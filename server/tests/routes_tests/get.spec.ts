00ed587936f346a5f7e91c5dc1bb97ad907905c3
SubhamJoshi18 <125031217+SubhamJoshi18@users.noreply.github.com>
2024-05-17 12:47:40 +0545

    testingbranch

--- server/tests/routes_tests/get.spec.ts
+++ server/tests/routes_tests/get.spec.ts
@@ -1,16 +1,3 @@
-import request from "supertest";
-import express from "express";
-
-describe("Test Routes Testing", () => {
-  let app: any;
-  beforeAll(() => {
-    app = express();
-  });
-  describe("Get Method for /api/test Testing", () => {
-    it("Should return an normal response", async () => {
-      const mock = jest.fn();
-      const response = await request(app).get("/api/test");
-      console.log(response.body);
-    });
-  });
-});
+import { app } from "../../src/index";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";

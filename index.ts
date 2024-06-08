75a6b678101924359746186d4ab721c58a1b6344
Subham Joshi <shubhamrajjoshi69@gmail.com>
2024-06-08 11:43:34 +0545

    my commit message

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
-import { App } from "./server";
-
-import envConfig from "./config/env.config";
-const serverPort = envConfig.serverPort;
-export const app = new App(serverPort);
-app.listen();
+my new file contents
